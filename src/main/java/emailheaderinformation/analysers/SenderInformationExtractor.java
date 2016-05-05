d9e1ead0b91f3c61180f86bf2d4d062d24eb9557
Joshua Clark <joshua@clark.io>
2016-05-05 12:41:14 +0100

    Collecting new information on sender

---
+++ src/main/java/emailheaderinformation/analysers/SenderInformationExtractor.java
@@ -0,0 +1,22 @@
+package emailheaderinformation.analysers;
+
+import emailheaderinformation.MainWindow;
+import emailheaderinformation.model.Header;
+
+public class SenderInformationExtractor extends HeaderAnalyser {
+    public SenderInformationExtractor (Header header, MainWindow mainWindow) {
+        super(header, mainWindow);
+    }
+
+    @Override public void run () {
+        String fromField = mHeader.getFields().get("From");
+        int emailStart = fromField.indexOf("<");
+        mMainWindow.getFoundInformation().setName(fromField.substring(0, emailStart - 1));
+        Object[] arr = {
+                "Personal", "Sender Information", "Yes", fromField.substring(0, emailStart - 1)
+        };
+        mMainWindow.addToTable(arr);
+
+
+    }
+}
