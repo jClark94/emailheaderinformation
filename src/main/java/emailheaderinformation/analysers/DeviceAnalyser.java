dd1158280ae22d23650f773ec45e3fbc90e0c32b
Joshua Clark <joshuaclark94@gmail.com>
2016-05-10 18:16:53 +0100

    Extract IP address from device nad use for location lookup

--- src/main/java/emailheaderinformation/analysers/DeviceAnalyser.java
+++ src/main/java/emailheaderinformation/analysers/DeviceAnalyser.java
@@ -4,9 +4,9 @@ import emailheaderinformation.MainWindow;
 import emailheaderinformation.model.Device;
 import emailheaderinformation.model.Header;
 
-/**
- * Created by jaclark on 09/05/16.
- */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DeviceAnalyser extends HeaderAnalyser {
 
   public DeviceAnalyser (Header header, MainWindow mainWindow) {
@@ -14,6 +14,25 @@ public class DeviceAnalyser extends HeaderAnalyser {
   }
 
   @Override public Device call () throws Exception {
+    Device device = mHeader.getStartDevice();
+
+    while (device.getNext() != null) {
+      String ip = "";
+      Pattern pattern = Pattern.compile("(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}" +
+                                        "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+      Matcher matcher = pattern.matcher(device.getName());
+      if (matcher.find()) {
+        ip = matcher.group(0);
+      }
+
+      GeoIPAnalyser geoIPAnalyser = new GeoIPAnalyser(mHeader, mMainWindow, ip);
+      String result = (String) mMainWindow.submitToExecutorService(geoIPAnalyser).get();
+      device.setLatitude(Float.parseFloat(result.split(",")[0]));
+      device.setLongitude(Float.parseFloat(result.split(",")[1]));
+
+      device = device.getNext();
+    }
+
     return null;
   }
 }
