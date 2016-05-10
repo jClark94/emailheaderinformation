1185f62616473f8873eed4668e0416fd1b5a139e
Joshua Clark <joshuaclark94@gmail.com>
2016-05-10 18:14:06 +0100

    Remove spaces to reflect now format

--- src/main/java/emailheaderinformation/parser/DateUtil.java
+++ src/main/java/emailheaderinformation/parser/DateUtil.java
@@ -11,14 +11,14 @@ public class DateUtil {
   // non-standard date formats to take into account spacing introduced in parser
   // module
   private static final SimpleDateFormat rfc822DateFormats[] = new SimpleDateFormat[] {
-      new SimpleDateFormat(" EEE , d MMM yy HH : mm : ss z"),
-      new SimpleDateFormat(" EEE , d MMM yy HH : mm z"),
-      new SimpleDateFormat(" EEE , d MMM yyyy HH : mm : ss z"),
-      new SimpleDateFormat(" EEE , d MMM yyyy HH : mm z"),
-      new SimpleDateFormat(" d MMM yy HH : mm z"),
-      new SimpleDateFormat(" d MMM yy HH : mm : ss z"),
-      new SimpleDateFormat(" d MMM yyyy HH : mm z"),
-      new SimpleDateFormat(" d MMM yyyy HH : mm : ss z") };
+      new SimpleDateFormat("EEE, d MMM yy HH:mm:ss z"),
+      new SimpleDateFormat("EEE, d MMM yy HH:mm z"),
+      new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z"),
+      new SimpleDateFormat("EEE, d MMM yyyy HH:mm z"),
+      new SimpleDateFormat("d MMM yy HH:mm z"),
+      new SimpleDateFormat("d MMM yy HH:mm:ss z"),
+      new SimpleDateFormat("d MMM yyyy HH:mm z"),
+      new SimpleDateFormat("d MMM yyyy HH:mm:ss z") };
 
   /**
    * Parse an RFC 822 date string.
