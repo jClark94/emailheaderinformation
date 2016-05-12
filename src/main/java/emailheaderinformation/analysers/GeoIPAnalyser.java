e310152423d67b83c1ae2b9164ca946db6f79804
Joshua Clark <joshuaclark94@gmail.com>
2016-05-12 22:00:09 +0100

    Better exception handling

--- src/main/java/emailheaderinformation/analysers/GeoIPAnalyser.java
+++ src/main/java/emailheaderinformation/analysers/GeoIPAnalyser.java
@@ -2,18 +2,22 @@ package emailheaderinformation.analysers;
 
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.DatabaseReader.Builder;
+import com.maxmind.geoip2.exception.AddressNotFoundException;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CityResponse;
 import emailheaderinformation.MainWindow;
 import emailheaderinformation.model.Header;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
 
-public class GeoIPAnalyser extends HeaderAnalyser<String> {
+class GeoIPAnalyser extends HeaderAnalyser<String> {
 
+  static final String NO_RESULT_FOUND = "NO_RESULT_FOUND";
   private final String mAddress;
 
-  public GeoIPAnalyser (Header header, MainWindow mainWindow, String ipAddress) {
+  GeoIPAnalyser (Header header, MainWindow mainWindow, String ipAddress) {
     super(header, mainWindow);
     mAddress = ipAddress;
   }
@@ -26,9 +30,11 @@ public class GeoIPAnalyser extends HeaderAnalyser<String> {
       return String.format("%s,%s",
                            city.getLocation().getLatitude(),
                            city.getLocation().getLongitude());
-    } catch (Exception ignored) {
-      ignored.printStackTrace();
+    } catch (AddressNotFoundException anfe) {
+      return NO_RESULT_FOUND;
+    } catch (IOException | GeoIp2Exception e) {
+      e.printStackTrace();
+      return null;
     }
-    return null;
   }
 }
