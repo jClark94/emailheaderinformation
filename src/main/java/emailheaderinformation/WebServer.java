39d0e4c36c256ad401b654b236cb2066dab44f0a
Joshua Clark <joshuaclark94@gmail.com>
2016-05-12 22:45:53 +0100

    Allows serving world map from resources

--- src/main/java/emailheaderinformation/WebServer.java
+++ src/main/java/emailheaderinformation/WebServer.java
@@ -4,13 +4,25 @@ import fi.iki.elonen.NanoHTTPD;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
 
-/**
- * Created by jaclark on 11/05/16.
- */
-public class WebServer extends NanoHTTPD {
+class WebServer extends NanoHTTPD {
   private String mPage;
+
+  /**
+   * Constructs an HTTP server on given port.
+   *
+   * @param port
+   */
+  WebServer (Component component, String page, int port) throws IOException {
+    super(port);
+    start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+    JOptionPane.showMessageDialog(component,
+                                  String.format("Running! Point your broswers to " +
+                                                "http://localhost:%d/", port));
+    mPage = page;
+  }
 
   /**
    * Override this to customize the server.
@@ -24,22 +36,27 @@ public class WebServer extends NanoHTTPD {
    * @return HTTP response, see class Response for details
    */
   @Override public Response serve (IHTTPSession session) {
-    return newFixedLengthResponse(mPage);
-
-  }
-
-  /**
-   * Constructs an HTTP server on given port.
-   *
-   * @param port
-   */
-  public WebServer (Component component, String page, int port) throws IOException {
-    super(port);
-    start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
-    JOptionPane.showMessageDialog(component,
-                                  String.format("Running! Point your broswers to " +
-                                                "http://localhost:%d/", port));
-    mPage = page;
+    switch (session.getUri()) {
+      case "/world-50m.json":
+        try {
+          File file = new File(this.getClass().getResource("/world-50m.json").toURI());
+          try (InputStream in = new FileInputStream(file)) {
+            try (InputStreamReader isr = new InputStreamReader(in)) {
+              try (BufferedReader br = new BufferedReader(isr)) {
+                StringBuilder sb = new StringBuilder();
+                br.lines().forEach(sb:: append);
+                return newFixedLengthResponse(sb.toString());
+              }
+            }
+          }
+        } catch (IOException | URISyntaxException e) {
+          e.printStackTrace();
+        }
+        break;
+      default:
+        return newFixedLengthResponse(mPage);
+    }
+    return null;
   }
 
 }
