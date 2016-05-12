540808424648768481ff7954cf54c6febb3180ff
Joshua Clark <joshuaclark94@gmail.com>
2016-05-12 22:01:43 +0100

    Change access permissions on StringChunker

--- src/main/java/emailheaderinformation/parser/StringChunker.java
+++ src/main/java/emailheaderinformation/parser/StringChunker.java
@@ -1,22 +1,22 @@
 package emailheaderinformation.parser;
 
-public class StringChunker {
+class StringChunker {
   private String[] words;
   private int position = 0;
 
-  public StringChunker(String s) {
+  StringChunker(String s) {
     words = s.split("\\s+");
   }
 
-  public boolean hasNext() {
+  boolean hasNext() {
     return words.length > position;
   }
 
-  public String peek() {
+  String peek() {
     return words[position];
   }
 
-  public String next() {
+  String next() {
     return words[position++];
   }
 }
