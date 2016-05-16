fce9eab3b30671c347ed6f00247fef35a3d49161
Joshua Clark <joshuaclark94@gmail.com>
2016-05-16 17:42:54 +0100

    Better lookup for IP addresses

--- src/main/java/emailheaderinformation/analysers/DeviceAnalyser.java
+++ src/main/java/emailheaderinformation/analysers/DeviceAnalyser.java
@@ -5,6 +5,8 @@ import emailheaderinformation.model.Device;
 import emailheaderinformation.model.Header;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,66 +15,85 @@ import static java.lang.Float.parseFloat;
 
 public class DeviceAnalyser extends HeaderAnalyser {
 
+  private Pattern pattern;
+  private Pattern hostname;
+
   public DeviceAnalyser (Header header, MainWindow mainWindow) {
     super(header, mainWindow);
   }
 
-  @Override public Device call () throws Exception {
+  @Override public Device call () throws ExecutionException {
     Device device = mHeader.getStartDevice();
 
-    Pattern pattern = Pattern.compile("([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})");
-    String
-        validHostnameRegex
-        = "(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*" +
-          "([A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])";
-    Pattern hostname = Pattern.compile(validHostnameRegex);
+    pattern = Pattern.compile("([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})");
+    String validHostnameRegex = "(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*" +
+                                "([A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])";
+    hostname = Pattern.compile(validHostnameRegex);
 
     while (device.getNext() != null) {
-      // Regex to match IP addresses, with some bounds checking
-      String ip = "";
-      float lat = 0;
-      float lon = 0;
-      Matcher matcher = pattern.matcher(device.getName());
-      while (matcher.find() && lat == 0 && lon == 0) {
-        String tempIp = matcher.group();
-        GeoIPAnalyser geoIPAnalyser = new GeoIPAnalyser(mHeader, mMainWindow, tempIp);
+      try {
+        String devName = device.getName();
+        extractInformation(devName, device);
+      } catch (InterruptedException | ExecutionException | UnknownHostException e) {
+        e.printStackTrace();
+      }
+
+      try {
+        extractInformation(device.getOrigin(), device);
+      } catch (InterruptedException | UnknownHostException e) {
+        e.printStackTrace();
+      }
+
+      device = device.getNext();
+    }
+
+
+    return null;
+  }
+
+  private void extractInformation (String devName, Device device)
+      throws InterruptedException, java.util.concurrent.ExecutionException, UnknownHostException {
+    // Regex to match IP addresses, with some bounds checking
+    String ip = "";
+    float lat = 0;
+    float lon = 0;
+    Matcher matcher = pattern.matcher(devName);
+    while (matcher.find() && lat == 0 && lon == 0) {
+      String tempIp = matcher.group();
+      GeoIPAnalyser geoIPAnalyser = new GeoIPAnalyser(mHeader, mMainWindow, tempIp);
+      String result = (String) mMainWindow.submitToExecutorService(geoIPAnalyser).get();
+      // Allow for local IP addresses being found
+      if (!NO_RESULT_FOUND.equals(result)) {
+        ip = matcher.group();
+        lat = parseFloat(result.split(",")[0]);
+        device.setLatitude(lat);
+        lon = parseFloat(result.split(",")[1]);
+        device.setLongitude(lon);
+      }
+    }
+
+    // Coincidentally, just above the middle of the Atlantic.  No servers here
+    if (lat == 0 && lon == 0) {
+      Matcher hostMatcher = hostname.matcher(devName);
+      if (hostMatcher.find()) {
+        String host = hostMatcher.group(0);
+        InetAddress inetAddress = InetAddress.getByName(host);
+        GeoIPAnalyser geoIPAnalyser = new GeoIPAnalyser(mHeader,
+                                                        mMainWindow,
+                                                        inetAddress.getHostAddress());
         String result = (String) mMainWindow.submitToExecutorService(geoIPAnalyser).get();
         // Allow for local IP addresses being found
         if (!NO_RESULT_FOUND.equals(result)) {
-          ip = matcher.group();
+          ip = inetAddress.getHostAddress();
           lat = parseFloat(result.split(",")[0]);
           device.setLatitude(lat);
           lon = parseFloat(result.split(",")[1]);
           device.setLongitude(lon);
         }
       }
-
-      // Coincidentally, just above the middle of the Atlantic.  No servers here
-      if (lat == 0 && lon == 0) {
-        Matcher hostMatcher = hostname.matcher(device.getName());
-        if (hostMatcher.find()) {
-          String host = hostMatcher.group(0);
-          InetAddress inetAddress = InetAddress.getByName(host);
-          GeoIPAnalyser geoIPAnalyser = new GeoIPAnalyser(mHeader, mMainWindow,
-                                                          inetAddress.getHostAddress());
-          String result = (String) mMainWindow.submitToExecutorService(geoIPAnalyser).get();
-          // Allow for local IP addresses being found
-          if (!NO_RESULT_FOUND.equals(result)) {
-            ip = inetAddress.getHostAddress();
-            lat = parseFloat(result.split(",")[0]);
-            device.setLatitude(lat);
-            lon = parseFloat(result.split(",")[1]);
-            device.setLongitude(lon);
-          }
-        }
-      }
-
-      mMainWindow.getVfm().lookupVulnerabilityForKeyword(device.getSoftware());
-      mMainWindow.getFoundInformation().addDevice(ip, lat, lon);
-
-      device = device.getNext();
     }
 
-    return null;
+    mMainWindow.getVfm().lookupVulnerabilityForKeyword(device.getSoftware());
+    mMainWindow.getFoundInformation().addDevice(ip, lat, lon, device.getSoftware());
   }
 }
