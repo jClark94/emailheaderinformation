59edc415344e295aab087e0d07d63d1c06f39337
Joshua Clark <joshuaclark94@gmail.com>
2016-05-16 17:46:53 +0100

    Add software field, remove getters and setters

--- src/main/java/emailheaderinformation/model/DeviceInformation.java
+++ src/main/java/emailheaderinformation/model/DeviceInformation.java
@@ -4,45 +4,14 @@ public class DeviceInformation {
   private int id;
   private String ipAddress;
   private float lat;
-
-  public float getLon () {
-    return lon;
-  }
-
-  public void setLon (float lon) {
-    this.lon = lon;
-  }
-
-  public float getLat () {
-    return lat;
-  }
-
-  public void setLat (float lat) {
-    this.lat = lat;
-  }
-
   private float lon;
+  private final String software;
 
-  public DeviceInformation (int id, String ip, float lat, float lon) {
+  public DeviceInformation (int id, String ip, float lat, float lon, String software) {
     this.id = id;
     this.ipAddress = ip;
     this.lat = lat;
     this.lon = lon;
-  }
-
-  public int getId () {
-    return id;
-  }
-
-  public void setId (int id) {
-    this.id = id;
-  }
-
-  public String getIpAddress () {
-    return ipAddress;
-  }
-
-  public void setIpAddress (String ipAddress) {
-    this.ipAddress = ipAddress;
+    this.software = software;
   }
 }
